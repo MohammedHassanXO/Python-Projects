b60d1d2eac020cffd6b94c48f5cbe6d4785d3b03
MohammedHassanXO <mohammedhassan12_5@yahoo.com>
2023-09-18 03:09:56 +0300

    finish task 5

---
+++ 01-task5.py
@@ -0,0 +1,10 @@
+"""
+Create a function that takes a sentence and prints how many words in the sentence (do not count the spaces)
+"""
+
+def sent() :
+    stringlist = input().split()
+    count = len(stringlist)
+    print(count)
+    return
+sent()
